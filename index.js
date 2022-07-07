5b3171cc7f6976003e8e70185399dcd37fc7b479
Kevin Alce <Kevinalce33@gmail.com>
2022-07-07 17:54:32 -0400

    commit tree 6f15c6618264a01df54b758f64ab5112df4eeaf0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+const CardNames = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cardNames){
+  for (let i = 0; i < cardNames.length; i++){
+    console.log(`Thank you, ${cardNames}, for the wonderful surprise gift!`)
+  }
+  return cardNames;
+}
+writeCards(cardNames)
